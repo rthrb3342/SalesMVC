38c91454a04f6757101e25801dc5578143a45ea5
Arthur Bert <arthurdbertoldo@gmail.com>
2026-01-26 16:07:49 -0300

    Foreign Key not null (referential integrity)

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,4 +1,6 @@
-﻿namespace SalesWebMVC.Models
+﻿using Microsoft.Build.Construction;
+
+namespace SalesWebMVC.Models
 {
     public class Seller
     {
@@ -8,6 +10,7 @@
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department? Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
