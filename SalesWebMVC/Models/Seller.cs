c3fb7501c2116ce8677d0acd2305df31dcbea6af
Arthur Bert <arthurdbertoldo@gmail.com>
2026-01-29 16:31:05 -0300

    Validation

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -6,18 +6,28 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} length must be a least {2} characters long")]
         public string? Name { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")] 
         public string? Email { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department? Department { get; set; }
         public int? DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
