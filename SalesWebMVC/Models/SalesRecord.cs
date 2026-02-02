220f29cf93a37eb3bf2725ed7c2ee86ac2136e6e
Arthur Bert <arthurdbertoldo@gmail.com>
2026-02-02 14:50:06 -0300

    Implementing grouping search

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -6,7 +6,11 @@ namespace SalesWebMVC.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double Amount { get; set; }
 
         public SalesStatus Status { get; set; }
